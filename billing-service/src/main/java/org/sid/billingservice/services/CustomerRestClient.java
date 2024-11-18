2e1626e07abefdefc479135511b0b151d6512eb9
chaimaaKaine <chaimaa.kaine@marketingconfort.com>
2024-11-18 18:31:48 +0100

    Last commit

--- billing-service/src/main/java/org/sid/billingservice/services/CustomerRestClient.java
+++ billing-service/src/main/java/org/sid/billingservice/services/CustomerRestClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
-    @GetMapping(path="/customers/{id}")
+    @GetMapping(path="/api/customers/{id}")
     Customer findCustomerById(@PathVariable Long id);
 
 }
--- customer-service/src/main/java/com/sid/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/sid/customerservice/CustomerServiceApplication.java
@@ -1,10 +1,12 @@
 package com.sid.customerservice;
 
+import com.sid.customerservice.config.CustomerConfigParams;
 import com.sid.customerservice.entities.Customer;
 import com.sid.customerservice.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -12,6 +14,7 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CustomerConfigParams.class)
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
